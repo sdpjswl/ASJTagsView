382ac6c3ad990772969688c816702b6316b6c31a
sudeep <sudeepjaiswal87@gmail.com>
2016-05-07 21:20:17 +0530

    added comments

--- ASJTags/ASJTags.h
+++ ASJTags/ASJTags.h
@@ -14,18 +14,52 @@ typedef void (^TagBlock)(NSString *tagText, NSInteger idx);
 
 @interface ASJTags : UIScrollView
 
+/**
+ *  Background color of tags.
+ */
 @property (strong, nonatomic) UIColor *tagColor;
+
+/**
+ *  Text color for tags' strings.
+ */
 @property (strong, nonatomic) UIColor *tagTextColor;
+
+/**
+ *  Text color for the cross (delete button).
+ */
 @property (strong, nonatomic) UIColor *crossColor;
+
+/**
+ *  Round the tags using this property.
+ */
 @property (assign, nonatomic) CGFloat cornerRadius;
+
+/**
+ *  Sets the spacing between the tags and between tags and the edges.
+ */
 @property (assign, nonatomic) CGFloat tagSpacing;
 
 @property (copy) TagBlock tapBlock;
 @property (copy) TagBlock deleteBlock;
 
+/**
+ *  Add a tag to the tags view.
+ */
 - (void)addTag:(NSString *)tag;
+
+/**
+ *  Delete all tags matching the given string.
+ */
 - (void)deleteTag:(NSString *)tag;
+
+/**
+ *  Delete the tag at the specified index.
+ */
 - (void)deleteTagAtIndex:(NSInteger)idx;
+
+/**
+ *  Reload the tags view.
+ */
 - (void)reloadTagsView;
 
 - (void)setTapBlock:(TagBlock)tapBlock;
